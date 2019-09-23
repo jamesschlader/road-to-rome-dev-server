2d43694526a6226e951a49e2f4ed4d21cdc35ceb
Jim Schlader <jamesschlader@yahoo.com>
2019-09-23 16:16:17 -0600

    trying to connect to mongodb

--- getDbUri.js
+++ getDbUri.js
@@ -12,7 +12,7 @@ const getDbUri = env => {
     case "production":
       return (
         process.env.PRODUCTION_DB_DSN ||
-        "mongodb://localhost:27017/road-to-rome-react"
+        "mongodb+srv://admin:p9b2p7SzQ%23FgsAc@cluster0-ducdn.mongodb.net/ROAD_TO_ROME_DEVELOPMENT?retryWrites=true&w=majority"
       );
     case "test":
       return (
