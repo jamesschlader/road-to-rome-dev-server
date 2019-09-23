f8b234a475b39a3d93a7da1bb8ea8c6ef89b7d87
Jim Schlader <jamesschlader@yahoo.com>
2019-09-23 13:22:17 -0600

    changed mongo access

--- getDbUri.js
+++ getDbUri.js
@@ -5,7 +5,7 @@ const getDbUri = env => {
     case "development":
       return (
         process.env.DEVELOPMENT_DB_DSN ||
-        "mongodb://localhost:27017/road-to-rome-react"
+        "mongodb+srv://admin:p9b2p7SzQ%23FgsAc@cluster0-ducdn.mongodb.net/"
       );
     case "production":
       return (
